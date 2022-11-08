abd1539af6423f9531410b6ab8fb0ce393d69f75
Sterling Carfrae <sterlinglc123@gmail.com>
2022-11-07 18:10:27 -0800

    working on resume

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,3 @@
+//import resume pdf file (from files) as variable into this file
+// as a tag where href of a tag is variable that made above
+// on a tag attribute: download = true
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -5,28 +5,30 @@ const Users=[
       picture: "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1600",
       heading: "PaperBoy",
       githubURL: "https://github.com/paullsshin/Paper-Boy",
-      description: "PaperBoy is a newsfeed app using Javascript, css, and html"
-    //   deployedLink:
+      description: "PaperBoy is a newsfeed app using Javascript, css, and html",
+      deployedLink: "https://paullsshin.github.io/Paper-Boy/"
     },
     {
         picture: "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1600",
         heading: "MoodRemedy",
         githubURL: "https://github.com/ccaitano/mood-remedy",
         description: "MoodRemedy is a mood based journalling app using Node.js, Chart.js, MySql, HandleBars, etc.",
-        deployedLink: "https://dashboard.heroku.com/apps/mood-remedy"
+        deployedLink: "https://mood-remedy.herokuapp.com/"
       },
       {
         picture: "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1600",
         heading: "MoodRemedy",
         githubURL: "https://github.com/ccaitano/mood-remedy",
-        description: "This card is a placeholder for now"
+        description: "This card is a placeholder for now",
+        deployedLink: "https://mood-remedy.herokuapp.com/"
       //   deployedLink:
       },
       {
         picture: "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1600",
         heading: "MoodRemedy",
         githubURL: "https://github.com/ccaitano/mood-remedy",
-        description: "This card is a placeholder for now"
+        description: "This card is a placeholder for now",
+        deployedLink: "https://mood-remedy.herokuapp.com/"
       //   deployedLink:
       }
   ]
