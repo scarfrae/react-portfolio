733922eb46c738f06b653843ee9eefd9185f85f0
Sterling Carfrae <sterlinglc123@gmail.com>
2022-11-14 23:57:14 -0800

    adding project

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -17,11 +17,11 @@ const Users=[
       },
       {
         picture: "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        heading: "MoodRemedy",
-        githubURL: "https://github.com/ccaitano/mood-remedy",
-        description: "This card is a placeholder for now",
+        heading: "MyLife",
+        githubURL: "https://github.com/ccaitano/my-life",
+        description: "MyLife is a mental health notification app that simplifies your life",
         deployedLink: "https://mood-remedy.herokuapp.com/"
-      //   deployedLink:
+      
       },
       {
         picture: "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1600",
